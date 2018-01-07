c228418f60a3f51153d20c9b3161b4c0741dc1b5
Drew C <drew.ciaramitaro51@gmail.com>
2018-01-06 21:32:18 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+import asyncio
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+        if message.content.startswith('!test'):
+            counter = 0
+            tmp = await message.channel.send('Calculating messages...')
+            async for msg in message.channel.history(limit=100):
+                if msg.author == message.author:
+                    counter += 1
+
+            await tmp.edit(content='You have {} messages.'.format(counter))
+        elif message.content.startswith('!sleep'):
+            with message.channel.typing():
+                await asyncio.sleep(5.0)
+                await message.channel.send('Done sleeping.')
+
+client = MyClient()
+tokenfile = open("token.dbot")
+token = tokenfile.read()
+tokenfile.close()
+client.run(token)
